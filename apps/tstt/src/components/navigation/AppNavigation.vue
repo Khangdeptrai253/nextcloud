d09225887bd652c953fc644265edf290810293cc
VinhNe <nguyenphucvinh1920@gmail.com>
2024-06-04 17:25:52 +0700

    [4] add icon for navigation item

--- apps/tstt/src/components/navigation/AppNavigation.vue
+++ apps/tstt/src/components/navigation/AppNavigation.vue
@@ -1,9 +1,21 @@
 <template>
 	<NcAppNavigation :class="{ 'icon-loading': loading }">
 		<template #list>
-			<NcAppNavigationItem :title="t('deck', 'Asset Management')" :exact="true" to="/" />
-			<NcAppNavigationItem :title="t('deck', 'Author Management')" :exact="true" to="/author" />
-			<NcAppNavigationItem :title="t('deck', 'Owner Management')" :exact="true" to="/owner" />
+			<NcAppNavigationItem :title="t('tstt', 'Asset classification')" :exact="true" to="/">
+				<template #icon>
+					<FolderOutline :size="20" />
+				</template>
+			</NcAppNavigationItem>
+			<NcAppNavigationItem :title="t('tstt', 'Copyright management')" :exact="true" to="/author">
+				<template #icon>
+					<ShieldAccountOutline :size="20" />
+				</template>
+			</NcAppNavigationItem>
+			<NcAppNavigationItem :title="t('tstt', 'Owner management')" :exact="true" to="/owner">
+				<template #icon>
+					<FolderAccountOutline :size="20" />
+				</template>
+			</NcAppNavigationItem>
 		</template>
 	</NcAppNavigation>
 </template>
@@ -11,9 +23,16 @@
 <script>
 import NcAppNavigation from '@nextcloud/vue/dist/Components/NcAppNavigation.js'
 import NcAppNavigationItem from '@nextcloud/vue/dist/Components/NcAppNavigationItem.js'
+import FolderOutline from 'vue-material-design-icons/FolderOutline.vue'
+import FolderAccountOutline from 'vue-material-design-icons/FolderAccountOutline.vue'
+import ShieldAccountOutline from 'vue-material-design-icons/ShieldAccountOutline.vue'
+
 export default {
 	name: 'AppNavigation',
 	components: {
+		FolderOutline,
+		FolderAccountOutline,
+		ShieldAccountOutline,
 		NcAppNavigation,
 		NcAppNavigationItem,
 	},
